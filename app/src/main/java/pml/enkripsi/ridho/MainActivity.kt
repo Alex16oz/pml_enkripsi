fdf17ba3bbc116749adfe95fd762f8fb77fc81a0
Alex16oz <85593164+Alex16oz@users.noreply.github.com>
2025-10-31 09:34:03 +0700

    feat: Add TopAppBar to MainActivity
    Adds a `TopAppBar` to the `MainActivity` screen.
    This includes setting the title to "PML Enkripsi" and applying a custom background color.

--- app/src/main/java/pml/enkripsi/ridho/MainActivity.kt
+++ app/src/main/java/pml/enkripsi/ridho/MainActivity.kt
@@ -6,20 +6,35 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import pml.enkripsi.ridho.ui.theme.PmlEnkripsiTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PmlEnkripsiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("PML Enkripsi") },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Color(0xFF03FCE8)
+                            )
+                        )
+                    }
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
